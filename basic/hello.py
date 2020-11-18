d89257957c8ae78e66cb0a848acc11c487eefa87
liuyuchen <49802081+liuyuchen777@users.noreply.github.com>
2020-11-18 12:20:08 +0800

    start

---
+++ basic/hello.py
@@ -0,0 +1,3 @@
+print("hello world!")
+print(2 ** 100) # 2 ** 100 不溢出
+print(8 * "spam!")
---
+++ basic/script1.py
@@ -0,0 +1,9 @@
+import sys
+print(sys.platform)
+print(2 ** 10000)
+x = 'spam!'
+print(x * 8)
+
+# save print result in txt
+# >> append
+# > save

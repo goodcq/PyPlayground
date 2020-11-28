019c75e87ee994acfda53f909ef15e3e815f5200
liuyuchen <liuyuchen.hust@gmail.com>
2020-11-28 16:42:18 +0900

    commit once

---
+++ basic/import_threenames.py
@@ -0,0 +1,5 @@
+import threenames
+print(threenames.a, threenames.b)
+from threenames import a, b, c
+print(a)
+print(dir(threenames))
--- basic/script1.py
+++ basic/script1.py
@@ -3,7 +3,8 @@ print(sys.platform)
 print(2 ** 10000)
 x = 'spam!'
 print(x * 8)
+input()
 
 # save print result in txt
 # >> append
-# > save
+# > cover
